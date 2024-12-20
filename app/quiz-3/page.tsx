08894e7afe5b9d023c02089a54d1a2e576f44c14
Henrique Gonçalves <hgoncalves@cnktd.com>
2024-12-20 18:51:50 -0300

    init

---
+++ app/quiz-3/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+import Confetti from 'react-confetti';
+
+import { useRef, useState } from 'react';
+import { useWindowSize } from '@/lib/use-window-size';
+
+import { ScratchCard } from 'next-scratchcard';
+
+export default function Page() {
+  const [canGoToNextStep, setCanGoToNextStep] = useState(false);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const { width, height } = useWindowSize();
+
+  const router = useRouter();
+
+  const handleComplete = () => {
+    setCanGoToNextStep(true);
+
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <div className="flex flex-col w-full items-center justify-center gap-4">
+      <h1 className="text-center text-3xl mb-16 mx-3">Raspe aqui para revelar a memória</h1>
+
+      <ScratchCard finishPercent={90} brushSize={30} height={200} onComplete={handleComplete}>
+        <img height="auto" width="100%" src="/foto-1.jpeg" />
+      </ScratchCard>
+
+      <Confetti width={width} height={height} run={canGoToNextStep} />
+
+      <audio src="/nostalgia.mp3" ref={audioRef} />
+
+      {canGoToNextStep && (
+        <div className="flex flex-col items-center justify-center my-4 gap-2">
+          <p className="w-full text-center">
+            Que saudade!! 👏👏 Além disso você ganhou um jantar surpresa
+          </p>
+          <Button onClick={() => router.push('quiz-4')}>Clique para ver</Button>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ app/quiz-5/page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+import { useRef } from 'react';
+
+export default function Page() {
+  const noButtonRef = useRef<HTMLButtonElement | null>(null);
+
+  const router = useRouter();
+
+  function changePosition() {
+    const i = Math.floor(Math.random() * 500) + 1;
+    const j = Math.floor(Math.random() * 500) + 1;
+
+    if (noButtonRef.current) {
+      noButtonRef.current.style.position = 'absolute';
+      noButtonRef.current.style.left = i + 'px';
+      noButtonRef.current.style.top = j + 'px';
+    }
+  }
+
+  return (
+    <div className="flex flex-col w-full items-center justify-center gap-4 relative">
+      <h1 className="text-center text-3xl mb-16 mx-3">
+        Agora pra desbloquear o último presente...
+      </h1>
+
+      <p className="w-full text-center underline text-lg">
+        Você promete não deixar de amar Henrique?
+      </p>
+
+      <div className="flex items-center gap-2">
+        <Button ref={noButtonRef} onClick={changePosition}>
+          Não
+        </Button>
+        <Button onClick={() => router.push('finish')}>Sim</Button>
+      </div>
+    </div>
+  );
+}
